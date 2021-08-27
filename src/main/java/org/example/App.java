0fa4f84d11276c05918a0ce64ffb18563ad0f2b9
brysonPaul <brysonpaul1870@gmail.com>
2021-08-27 16:13:36 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String noun, verb, adjective, adverb;
+        noun = strIO("Enter a noun: ", sc);
+        verb = strIO("Enter a verb: ", sc);
+        adjective = strIO("Enter an adjective: ", sc);
+        adverb = strIO("Enter an adverb: ", sc);
+        System.out.println("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+        sc.close();
+    }
+
+    public static String strIO(String input, Scanner sc) {
+        System.out.print(input);
+        String output = sc.nextLine();
+        return output;
+    }
+}
